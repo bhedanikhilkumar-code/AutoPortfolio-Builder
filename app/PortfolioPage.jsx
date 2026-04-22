3b57c9ee2ac74d1f643f58306f5d93c90d070420
Bheda Nikhilkumar <226649017+bhedanikhilkumar-code@users.noreply.github.com>
2026-04-22 07:42:56 +0100

    Add UI improvements

---
+++ app/PortfolioPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Styles.css';
+
+const PortfolioPage = () => {
+  return (
+    <section className="portfolio">
+      <h2>My Portfolio</h2>
+      {/* Add portfolio items here */}
+    </section>
+  );
+};
+
+export default PortfolioPage;
